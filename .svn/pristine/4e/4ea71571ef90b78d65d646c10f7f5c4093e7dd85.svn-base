e5258316ff92aebcd2d297a9082698e4c1113e9e
shihua-guo <shihua-guo@hotmail.com>
2016-11-29 10:11:07 +0800

    电子商店。

---
+++ .svn/pristine/4e/4ea71571ef90b78d65d646c10f7f5c4093e7dd85.svn-base
@@ -0,0 +1,9 @@
+package com.lyq.dao.product;
+
+import com.lyq.dao.BaseDao;
+import com.lyq.model.product.ProductCategory;
+
+public interface ProductCategoryDao extends BaseDao<ProductCategory>
+{
+
+}
---
+++ src/com/lyq/util/hibernate/PaymentWayType.java
@@ -0,0 +1,16 @@
+package com.lyq.util.hibernate;
+
+import com.lyq.model.PaymentWay;
+
+/**
+ * 支付方式Hibernate映射类型
+ * 
+ * @author Li Yongqiang
+ */
+public class PaymentWayType extends EnumType<PaymentWay>
+{
+	public PaymentWayType()
+	{
+		super(PaymentWay.class);
+	}
+}
